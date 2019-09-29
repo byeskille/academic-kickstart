2cf9b1ae1a91cd1f1467f0a89692a1570707544a
byeskille <oyvind@byeskille.no>
2019-09-29 16:15:52 +0200

    add wp arkiv

---
+++ arkiv/scripts/calculateage.js
@@ -0,0 +1,19 @@
+function calculateAge(birthMonth, birthDay, birthYear)
+{
+  todayDate = new Date();
+  todayYear = todayDate.getFullYear();
+  todayMonth = todayDate.getMonth();
+  todayDay = todayDate.getDate();
+  age = todayYear - birthYear; 
+
+  if (todayMonth < birthMonth - 1)
+  {
+    age--;
+  }
+
+  if (birthMonth - 1 == todayMonth && todayDay < birthDay)
+  {
+    age--;
+  }
+  return age;
+}
---
+++ arkiv/wp-content/uploads/minit/56f0a28b522a985be5c90b016e9775b4.js
@@ -0,0 +1,20 @@
+
+
+/* Minit: https://byeskille.net/wp-content/plugins/akismet/_inc/form.js */
+jQuery( function ( $ ) {
+	var ak_js = $( '#ak_js' );
+
+	// If the form field already exists just use that
+	if ( ak_js.length == 0 ) {
+		ak_js = $( '<input type="hidden" id="ak_js" name="ak_js" />' );
+	}
+	else {
+		ak_js.remove();
+	}
+
+	ak_js.val( ( new Date() ).getTime() );
+
+	// single page, front-end comment form
+	// inline comment reply, wp-admin
+	$( '#commentform, #replyrow td:first' ).append( ak_js );
+} );
